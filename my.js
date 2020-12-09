09aa9cf582c72e49918392cce1e7963c860e0019
Olya1999 <73253360+Olya1999@users.noreply.github.com>
2020-12-10 00:11:06 +0300

    aa

--- my.js
+++ my.js
@@ -20,29 +20,3 @@ function square() {
     }
     sol.innerHTML = str;
   }
-  let h=document.getElementById("h");
-  let m=document.getElementById("m");
-  let s=document.getElementById("s");
-  let da=document.getElementById("da");
-  setInterval(tic,1000);
-  function tic() {
-    let day = new Date();
-    let timeend = new Date("December 31, 2016");
-    let da = timeend - day.getDay();
-    let hours=23-day;
-    if (hours<10) {
-      hours="0"+hours;
-    }
-    let min=59-day.getMinutes();
-    if (min<10) {
-      min="0"+min;
-    }
-    let sec=59-day.getSeconds();
-    if (sec<10) {
-      sec="0"+sec;
-    }
-    da.innerHTML= da + ":";
-    h.innerHTML= hours+":";
-    m.innerHTML= min+":";
-    s.innerHTML= sec;
-  }
